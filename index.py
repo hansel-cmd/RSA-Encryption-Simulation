b3c61688a8108f4ea31e2a0046bf8d2829859411
Hansel <valkirecesa@gmail.com>
2021-11-09 00:07:45 +0800

    reduced possible numbers that are not factors of z to only numbers that satisfy certain condition

--- index.py
+++ index.py
@@ -8,20 +8,61 @@ def encrypt(plain_text, p, q):
 
     z_factors = get_factors_of_z(z)
     
+
     possible_numbers = [i for i in range(1, n) if i not in z_factors]
     
     # choose number e, less than n, 
     # which has no common factor with z
     print(possible_numbers)
-    while True:
-        e = input("Choose a number in the list: ")
-        try:
-            e = int(e)
-            if e in possible_numbers: break
-            else: print("Number is not in the list.")
-        except ValueError:
-            print("Number is invalid.")
     
+    # we need to remove numbers in the list that do not have any number that
+    # satisfies the next step: 
+    # where we find d, such that ed - 1 is exactly divisible by z
+    # example: n = 35, z = 24
+    # numbers in the list include 9 and 10 (as these numbers have no common 
+    # factor with z = 24), but 9 and 10 do not have the 'd' that will satisfy 
+    # the "ed - 1 is exactly divisible by z" condition
+    # even if we loop until 10,000.
+    
+    new_possible_numbers = []
+    for i in possible_numbers:
+        temp = []
+        for j in range(1, 100):
+            if ((i * j) - 1) % z == 0:
+                temp.append(j)
+        if temp:
+            new_possible_numbers.append(i)
+
+    print(new_possible_numbers)
+
+
+    # while True:
+    #     e = input("Choose a number in the list: ")
+    #     try:
+    #         e = int(e)
+    #         if e in possible_numbers: break
+    #         else: print("Number is not in the list.")
+    #     except ValueError:
+    #         print("Number is invalid.")
+    
+    # print(f"e is {e}")
+
+    # find number d, such that ed - 1 is exactly divisible by z
+    # ed - 1
+    # 9 * d - 1 
+
+    # z = 24
+    # for i in range (1, 100):
+    #     if ((e * i) - 1) % z == 0:
+    #         print(i)
+
+    # my_z = [z * i for i in range (2, 1000, 2)]
+    # print(my_z)
+
+    # my_ed = [(e * i) - 1 for i in range (1, 1000)]
+    # print(my_ed)
+
+
     
     
     
