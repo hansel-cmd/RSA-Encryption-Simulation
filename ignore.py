91a3a41dfdfc5cfede974b7c5ecb7da0abb6d025
Hansel <valkirecesa@gmail.com>
2021-11-10 00:03:58 +0800

    ignore ignore.py

--- ignore.py
+++ ignore.py
@@ -1,17 +1,11 @@
 ##### Ignore this File #####
 
-
-
 from helper import Replace
 from helper import Highlight
     
-
-
-
 p = 2
 q = 7
 n = 14
-# phi
 z = 6
 
 
@@ -19,7 +13,26 @@ private_key_d = 11
 private_key_n = 14
 private_key_e = 5
 
-cipher_text = input("Enter shit")
+# 73 ** 7 % 143 % 127 = 83
+# 83 ** 223 % 143 % 127 = 73
+
+
+# 108 ** 7 % 143 = 4
+# 4 ** 223 % 143 = 108
+
+# p = 11, q = 13
+# public_key: {'n': 143, 'e': 7}
+# private_key: {'n': 143, 'd': 223}
+# ['E', 'A', '\x08', 'H', 'A', 'M', 'A', 'N']
+
+
+# p = 11, q = 19
+# n = 209, e = 7, d = 283
+# 108 ** 7 % 209 = 48
+# 48 ** 283 % 209 = 108
+# \x11\n0\x02\x12"\n\x03\x14\x15&\x02"0\x14
+
+cipher_text = input("Enter something XD")
 cipher_text = ascii(cipher_text.encode().decode('unicode_escape'))
 
 # Since a string will typically use escape character,
@@ -50,40 +63,8 @@ print(f"{test_i} | {ord(test_i)}")
 print(f"{test_i_2} | {chr(ord(test_i_2))}")
 print(f"{test_i_3} | {ord(test_i_3)}")
 
-# 73 ** 7 % 143 % 127 = 83
-# 83 ** 223 % 143 % 127 = 73
-
-
-# 108 ** 7 % 143 = 4
-# 4 ** 223 % 143 = 108
-
-# p = 11, q = 13
-# public_key: {'n': 143, 'e': 7}
-# private_key: {'n': 143, 'd': 223}
-# ['E', 'A', '\x08', 'H', 'A', 'M', 'A', 'N']
-
-
-
-# p = 11, q = 19
-# n = 209, e = 7, d = 283
-# 108 ** 7 % 209 = 48
-# 48 ** 283 % 209 = 108
-# \x11\n0\x02\x12"\n\x03\x14\x15&\x02"0\x14
-
-
 
 # print(chr(ord(17)))
 for i in range (0, 128):
     print(ascii(chr(i)))
-
-
-
-# print(f"{Highlight.YELLOW}Encrypted message letters:", cipher_text)
-# print(f"{Highlight.YELLOW}Decrpyted message letters:", plain_text)
-
-# print(f"{Highlight.GREEN}Encrypted message:", ascii(''.join(cipher_text)))
-# print(f"{Highlight.GREEN}Decrypted message: '{''.join(plain_text)}'")
-
-
-
 
